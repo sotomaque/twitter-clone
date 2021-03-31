28dbc68c9ead68a1de725011cfc8cacfaa290cf9
Enrique <sotomaque@gmail.com>
2021-03-30 22:01:11 -0700

    fix: broken header / footer links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,60 +1,62 @@
-import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter";
-import { faBell } from "@fortawesome/free-regular-svg-icons/faBell";
-import { faComments } from "@fortawesome/free-regular-svg-icons/faComments";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons/faEnvelope";
-import { faListAlt } from "@fortawesome/free-regular-svg-icons/faListAlt";
-import { faUser } from "@fortawesome/free-regular-svg-icons/faUser";
-import { faEllipsisH } from "@fortawesome/free-solid-svg-icons/faEllipsisH";
-import { faHashtag } from "@fortawesome/free-solid-svg-icons/faHashtag";
-import { faHome } from "@fortawesome/free-solid-svg-icons/faHome";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { Badge, Col } from "react-bootstrap";
-import { useQuery } from "react-query";
-import MediaQuery from "react-responsive";
-import { Link, NavLink } from "react-router-dom";
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faBell } from '@fortawesome/free-regular-svg-icons/faBell';
+import { faComments } from '@fortawesome/free-regular-svg-icons/faComments';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+import { faListAlt } from '@fortawesome/free-regular-svg-icons/faListAlt';
+import { faUser } from '@fortawesome/free-regular-svg-icons/faUser';
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons/faEllipsisH';
+import { faHashtag } from '@fortawesome/free-solid-svg-icons/faHashtag';
+import { faHome } from '@fortawesome/free-solid-svg-icons/faHome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useAuthUser } from 'context/auth-context';
+import React from 'react';
+import { Badge, Col } from 'react-bootstrap';
+import { useQuery } from 'react-query';
+import MediaQuery from 'react-responsive';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
+  const authUser = useAuthUser();
   const list = [
     {
-      name: "Home",
-      href: "/home",
+      name: 'Home',
+      href: '/home',
       icon: faHome,
     },
     {
-      name: "Explore",
-      href: "/explore",
+      name: 'Explore',
+      href: '/explore',
       icon: faHashtag,
     },
     {
-      name: "Profile",
-      href: `/user/authuser-screen-name`,
+      name: 'Profile',
+      href: `/user/${authUser.screen_name}`,
       icon: faUser,
     },
     {
-      name: "Notifications",
-      href: "/notifications",
+      name: 'Notifications',
+      href: '/notifications',
       icon: faBell,
       count: 0,
     },
     {
-      name: "Chat Room",
-      href: "/chats",
+      name: 'Chat Room',
+      href: '/chats',
       icon: faComments,
     },
     {
-      name: "Settings",
-      href: "/settings",
+      name: 'Settings',
+      href: '/settings',
       icon: faEllipsisH,
     },
     {
-      name: "Messages",
-      href: "/messages",
+      name: 'Messages',
+      href: '/messages',
       icon: faEnvelope,
     },
     {
-      name: "Lists",
-      href: "/lists",
+      name: 'Lists',
+      href: '/lists',
       icon: faListAlt,
     },
   ];
@@ -76,7 +78,7 @@ export default function Header() {
               <Badge
                 className="position-absolute"
                 variant="primary"
-                style={{ top: 5, right: 5, left: "unset" }}
+                style={{ top: 5, right: 5, left: 'unset' }}
               >
                 {item.count}
               </Badge>
@@ -110,7 +112,7 @@ export default function Header() {
         <MediaQuery maxWidth={1199}>
           <svg
             viewBox="0 0 24 24"
-            style={{ height: 35, padding: 5, fill: "white" }}
+            style={{ height: 35, padding: 5, fill: 'white' }}
           >
             <path d="M8.8 7.2H5.6V3.9c0-.4-.3-.8-.8-.8s-.7.4-.7.8v3.3H.8c-.4 0-.8.3-.8.8s.3.8.8.8h3.3v3.3c0 .4.3.8.8.8s.8-.3.8-.8V8.7H9c.4 0 .8-.3.8-.8s-.5-.7-1-.7zm15-4.9v-.1h-.1c-.1 0-9.2 1.2-14.4 11.7-3.8 7.6-3.6 9.9-3.3 9.9.3.1 3.4-6.5 6.7-9.2 5.2-1.1 6.6-3.6 6.6-3.6s-1.5.2-2.1.2c-.8 0-1.4-.2-1.7-.3 1.3-1.2 2.4-1.5 3.5-1.7.9-.2 1.8-.4 3-1.2 2.2-1.6 1.9-5.5 1.8-5.7z" />
           </svg>
